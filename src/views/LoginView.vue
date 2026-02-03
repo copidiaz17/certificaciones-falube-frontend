a76023d38b3b4ba0bca4e0882281a8fb92f83c9a
copidiaz17 <copidiaz17@gmail.com>
2026-02-03 04:39:17 -0300

    ultimos cambios

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,27 @@
+<template>
+  <div class="login-page">
+    <div class="login-container">
+      <h1 class="login-title">SISTEMA DE CERTIFICACIÓN DE OBRA</h1>
+
+      <form @submit.prevent="login" class="login-form">
+        <img src="/falube.jpg" alt="Logo" class="logo" />
+
+        <div class="input-group">
+          <input v-model="email" type="email" placeholder="Email" required />
+        </div>
+
+        <div class="input-group">
+          <input v-model="password" type="password" placeholder="Contraseña" required />
+        </div>
+
+        <button type="submit" class="btn-submit">Ingresar</button>
+
+        <p v-if="error" class="error">{{ error }}</p>
+      </form>
+    </div>
+  </div>
+</template>
+
 <script>
 import api from "../config/axios.Config.js";
 import { useAuthStore } from "../stores/authStore";
@@ -52,3 +76,12 @@ export default {
   },
 };
 </script>
+
+
+<style scoped>
+/* NOTA: Estos estilos son mínimos, el resto debe estar en login.css */
+img {
+  max-width: 200px;
+  height: auto;
+}
+</style>
